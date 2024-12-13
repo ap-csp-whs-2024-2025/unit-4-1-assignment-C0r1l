7bb11d0c03e6c0232450fb266c13d2185caf3a5b
C0r1l <carolygonzalez1458@gmail.com>
2024-12-13 22:46:15 +0000

    kawaii

--- classwork.cpp
+++ classwork.cpp
@@ -15,14 +15,43 @@ int main()
 //Exercise 1
   int x = 25;
   int y =3;
+
   int add_xy;
   int sub_xy;
 
   add_xy = x + y;
   sub_xy = x - y;
-  MOD_xy = x % y;
 
-  std::cout << add_xy << std::endl << sub_xy << std::endl << MOD_xy << std::endl;
+  std::cout << add_xy << std::endl << sub_xy << std::endl << x % y << std::endl;
+
+  int z = 10;
+  
+    
+
+//Exercise 2
+
+  bool isSunny = true;
+  bool hasUmbrella = false;
+
+  std::cout << "IsSunny and hasUmbrella: " << (isSunny && hasUmbrella) << std::endl;
+  std::cout << "isSunny or hasUmbrella: "  << (isSunny || hasUmbrella) << std::endl;
+  std::cout << "isSunny is: " << (!isSunny) << std::endl;
+  std::cout << "IsNotSunny and IsNotUmbrella: " << (!isSunny && !hasUmbrella) << std::endl;
+
+
+  //Exercise 3
+
+  int n = 7;
+  double m = 4.0;
+  bool hasLicense = false;
+
+  std::cout << "N is greater than M?: " << (n > m) << std::endl;
+  
+
+
+
+
+
 
  
 
